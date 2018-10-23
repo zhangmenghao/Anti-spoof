370318c4d66b09d0934a1407c56b320441ba313c
Dracula <dracula.register@gmail.com>
2018-10-23 15:52:21 +0800

    Move the development to the server...

--- cache_version/switch/bmv2/env.sh
+++ cache_version/switch/bmv2/env.sh
@@ -1,2 +1,2 @@
-BMV2_PATH=/home/dracula/Downloads/p4_environment/behavioral-model
-P4C_BM_PATH=/home/dracula/Downloads/p4_environment/p4c-bm
+BMV2_PATH=/home/dracula/p4_environment/behavioral-model
+P4C_BM_PATH=/home/dracula/p4_environment/p4c-bm

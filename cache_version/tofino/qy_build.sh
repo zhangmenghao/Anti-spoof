cff9ecb9ca8b98eac192a544309127d1d7e881ff
kongxiao0532 <kongxiao0532@gmail.com>
2018-11-01 22:51:52 +0800

    Modify build script on tofino

--- cache_version/tofino/qy_build.sh
+++ cache_version/tofino/qy_build.sh
@@ -14,5 +14,5 @@ make -j8
 echo 'make done!'
 make install
 echo 'make install done!'
-sed -e "s/TOFINO_SINGLE_DEVICE/nethcf/" $SDE/pkgsrc/p4-examples/tofino/tofino_single_device.conf.in > $SDE_INSTALL/share/p4/targets/tofino/synproxy.conf
+sed -e "s/TOFINO_SINGLE_DEVICE/nethcf/" $SDE/pkgsrc/p4-examples/tofino/tofino_single_device.conf.in > $SDE_INSTALL/share/p4/targets/tofino/nethcf.conf
 echo 'conf done!'
